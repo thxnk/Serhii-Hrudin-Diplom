4d7c3662bb8a78d31cdad1ad75929b77ac5f01ed
Hrudin Serhii <grudin.svs@gmail.com>
2022-05-07 18:26:37 +0300

    Huge changes to version 1

---
+++ Diplom/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+
+    // Update is called once per frame
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Debug.Log("Died");
+    }
+}
